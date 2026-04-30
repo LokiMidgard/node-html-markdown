9835b4fd75a672fbf4eff6fde6f1fac630fda40f
Patrick Kranz <patrick-kranz@live.de>
2026-04-30 08:52:34 +0200

    Fix ReferenceError: process is not defined when in browser

--- src/utilities.ts
+++ src/utilities.ts
@@ -225,11 +225,11 @@ export function getChildNodes(node: HtmlNode | Node): (Node | HtmlNode)[] {
 }
 
 export function perfStart(label: string) {
-  if (process.env.LOG_PERF) console.time(label);
+  if (typeof process !== 'undefined' && process?.env?.LOG_PERF) console.time(label);
 }
 
 export function perfStop(label: string) {
-  if (process.env.LOG_PERF) console.timeEnd(label);
+  if (typeof process !== 'undefined' && process?.env?.LOG_PERF) console.timeEnd(label);
 }
 
 // endregion
